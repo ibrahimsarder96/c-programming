fef669f220369069e83ef2a6d622461579df434b
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-04-01 18:04:22 +0600

    c programming setup

---
+++ Setup/First.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int sub;
+    a = 75;
+    b = 25;
+    sub = a - b;
+    printf("Sub is %d", sub);
+    return  0;
+}
---
+++ Setup/sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int x;
+  int y;
+  int sum;
+  x = 50;
+  y = 20;
+  sum = x + y;
+  printf ("sum is %d", sum);
+  return  0;
+}
