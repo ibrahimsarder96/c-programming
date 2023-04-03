9db32bcf341b1f51b64c6f546219fd195e1e58be
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-04-03 22:33:21 +0600

    practice arithmetic math

---
+++ PracticeDay-1/practice1.2.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int main()
+{
+  int a, b, multiplication;
+  scanf("%d",& a);
+  scanf("%d",& b);
+  multiplication = a * b;
+  printf("%d", multiplication);
+  return 0;
+}
+// Division--------------
+/*
+#include<stdio.h>
+int main()
+{
+  int a, b, division;
+  scanf("%d",& a);
+  scanf("%d",& b);
+  division = a + b;
+  printf("%d", division);
+  return 0;
+}
+*/
+// Subtraction--------------
+/*
+#include<stdio.h>
+int main()
+{
+  int a, b, subtraction;
+  scanf("%d",& a);
+  scanf("%d",& b);
+  subtraction = a + b;
+  printf("%d", subtraction);
+  return 0;
+}
+*/
+// Addition------------------
+/*
+#include<stdio.h>
+int main()
+{
+  int a, b, summation;
+  scanf("%d",& a);
+  scanf("%d",& b);
+  summation = a + b;
+  printf("%d", summation);
+  return 0;
+}
+*/
