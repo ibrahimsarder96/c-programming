126ca3890581cd52314e3359ed774655294a05f0
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-04-04 22:50:14 +0600

    loop condition add

---
+++ Module-3(Loop)/loop_condition.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int i, n;
+  scanf("%d", &n);
+  for(i = 0;i <= n; i++)
+  {
+    if(i%2 == 0){
+      printf("%d-Even\n", i);
+    }
+    else{
+      printf("%d-Odd\n", i);
+    }
+  }
+  return 0;
+}
