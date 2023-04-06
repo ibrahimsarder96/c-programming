ab803be8a572a5f95ad4fd263f3545fb4d025b5c
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-04-06 21:08:09 +0600

    assignment problem1 done

--- PracticeDay-2/infinite-loop.c
+++ PracticeDay-2/infinite-loop.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
-int  main()
-{
-  int i;
-  for(i = 0; i >= 0; i++){
-    printf("An infinite loop -- sometimes called an endless loop -- is a piece of code that lacks...", i);
-  }
-  return 0;
-}
+
+// #include<stdio.h>
+// int  main()
+// {
+//   int i;
+//   for(i = 0; i >= 0; i++){
+//     printf("An infinite loop -- sometimes called an endless loop -- is a piece of code that lacks...", i);
+//   }
+//   return 0;
+// }
--- PracticeDay-2/loop.c
+++ PracticeDay-2/loop.c
@@ -1,16 +1,44 @@
-#include<stdio.h>
-int main()
-{
-  int i, n;
-  scanf("%d", &n);
-  for(i = 0;i <= n; i++)
-  {
-    if(i%2 == 0){
-      printf("%d-Even\n", i);
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int i;
+    int n;
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++){
+      if(i % 5 == 0){
+          printf("%d Yes\n", i);
+    }else if(i){
+        printf("%d No\n", i);  
+      }
+        
+      
     }
-    else{
-      printf("%d-Odd\n", i);
-    }
-  }
-  return 0;
+    return 0;
 }
+/*
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    // Enter your code here. Read input from STDIN. Print output to STDOUT    
+    int a;
+    long long int b;
+    float c;
+    char d;
+    scanf("%d", &a);
+    scanf("%lld", &b);
+    scanf("%f", &c);
+    scanf("\n%c", &d);
+
+    printf("%d\n%lld\n%0.4f\n%c", a,b,c,d);
+    return 0;
+}
+*/
