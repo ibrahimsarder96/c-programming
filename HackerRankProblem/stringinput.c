a2a9ecac6a08c8a7ba0548de13070190296737a9
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-04-07 11:54:26 +0600

    HackerRank first problem done

---
+++ HackerRankProblem/stringinput.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+      char str[500];
+    scanf("%[^\n]%*c", str);
+    printf("Hello, World! \n%s", str);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
