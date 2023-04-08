86e7c7f0279a95a94101683aacc2d755ffa1402a
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-04-08 21:08:58 +0600

    module4 problemi done

--- HackerRankProblem/largeNumber.c
+++ HackerRankProblem/largeNumber.c
@@ -6,7 +6,6 @@ Add `int max_of_four(int a, int b, int c, int d)` here.
 int main() {
     int a, b, c, d;
     scanf("%d %d %d %d", &a, &b, &c, &d);
-    // int ans = max_of_four(a, b, c, d);
     if(a > b && a > c && a > d){
         printf("%d\n", a);
     }
