e19bc9546c819aedb13ebf03a1907f09c39407df
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-04-11 23:44:14 +0600

    array total sum

--- Module-7(Array)/initilization.c
+++ Module-7(Array)/initilization.c
@@ -3,7 +3,7 @@ int main()
 {
     int ar[5] = {2, 4, 5, 6, 3};
     // int ar[5] = {2}; // decler na korle baki sob zero hobe
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < 3; i++)
     {
       printf("%d\n", ar[i]);
     }
---
+++ Module-7(Array)/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+  // for(int i = 0; i < 3; i++)
+    // {
+    //   // printf("%d\n", ar[i]);
+    //   // printf("%d\n", arrayName[1]);
+      
+    // }
