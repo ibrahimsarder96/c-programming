9a924ea053b4f2bed17ff729af001f2886139994
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-04-16 23:51:43 +0600

    string  print korar super power

--- Module-10(Array-String)/array-char.c
+++ Module-10(Array-String)/array-char.c
@@ -4,12 +4,12 @@ int main()
     // array and stirng er size ber kora holo below
     char c[9];
     int arr[7];
-    int sz = sizeof(c)/sizeof(char);
-    int siz = sizeof(arr);
+    int sz = sizeof(c);
+    int siz = sizeof(arr)/sizeof(int);
     printf("%d\n", sz);
     printf("%d\n", siz);
-    
-    // akdik string print
+
+    // akadik string print
     char name[7];
     for(int i = 0; i < 7; i++)
     {
---
+++ Module-10(Array-String)/initialize.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    // char a[7] = {'i', 'b', 'r', 'h', 'i', 'm'};
+    
+    char  w[11] = "Bangladesh"; //string print korar somoy size 1 bariye dite hoy je ta nul value hisabe kaj kore thake r ati holo print  korar super power
+    
+    // for(int i  = 0; i < 7;  i++)
+    // {
+    // printf("%s\n", a);
+    // }
+   printf("%s\n", w);
+    return 0;
+}
