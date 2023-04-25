92cc2904710ef465fd8765ecb59525ee26f3b15f
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-04-25 22:32:09 +0600

    problem6 done

---
+++ Module-12MidtermExam/problem-6.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+      scanf("%d", &arr[i]);
+    }
+    int v;
+    scanf("%d", &v);
+    int cnt=0;
+    for(int i=0; i<n; i++)
+    {
+      if(arr[i]==v)
+      {
+        cnt++;
+      }
+    }
+    printf("%d", cnt);
+    return 0;
+}
