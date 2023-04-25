263f6eb2c326f9f2bdc09a7574fa9e15f2b99315
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-04-25 22:19:47 +0600

    problem3 done

---
+++ Module-12MidtermExam/problem-5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n, k;
+    scanf("%d %d", &n, &k);
+    for(int i=0; i<k;i++)
+    {
+         scanf("%d", &k);
+        printf("%d", i);
+      
+    }
+     for(int j=1; j<=n; j++)
+    {
+      scanf("%d", &n);
+       printf("%d\n", j);
+    }
+    return 0;
+}
