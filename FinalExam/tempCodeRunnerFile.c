c596e4b572df3f273573ab9015666af2718376fb
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-05-12 11:23:06 +0600

    twodmatrix done

---
+++ FinalExam/tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+for(int i = 0; i < col; i++)
+    // {
+    //   printf("%d", a[0][i]);
+    // }
+    //   printf("\n");
+    //  for(int i = 0; i < row; i++)
+    // {
+    //   printf("%d", a[i][2]);
+    // }
